0800581c0cf7d4c3f6bb2f82371b044f503fbed2
Valentyn Ziomenko <ziomenkow@gmail.com>
2026-09-12 00:04:23 +0200

    add 1 midleware and test error

--- src/server.js
+++ src/server.js
@@ -1,11 +1,8 @@
 import express from "express";
 import helmet from "helmet";
-import path from "node:path";
 import "dotenv/config";
 import cors from "cors";
 import logger from "pino-http";
-
-const errorPath = path.join(import.meta.dirname, "test", "error.js");
 
 const app = express();
 
@@ -15,22 +12,24 @@ app.use(cors());
 
 app.use(logger());
 
+app.use(express.json());
+
 app.get("/notes", (req, res) => {
  res.status(200).json({
-  succsess: true,
-  message: "notes fetch succsessfully",
+	message: "Retrieved all notes",
  });
 })
 
-app.get("/notes:noteId", (req, res) =>{
+app.get("/notes/:noteId", (req, res) =>{
   const noteID = req.params.noteId;
   res.status(200).json({
     message: `Retrieved note with ID:${noteID}`
   });
 })
 
-app.get(errorPath, (req, res) =>{
-})
+app.get("/test/error.js",  (req, res, next) => {
+  next(new Error("Test error"));
+});
 
 app.use((req, res, next) => {
   res.status(404).json({message: "Route not found"});
@@ -40,7 +39,7 @@ app.use((error, req, res, next) =>{
   res.status(500).json({message: error.message});
 })
 
-app.listen(process.env.PORT, () =>{
+app.listen(process.env.PORT || 3000, () =>{
   console.log(`Server is running on PORT=${process.env.PORT}`);
 })
 
