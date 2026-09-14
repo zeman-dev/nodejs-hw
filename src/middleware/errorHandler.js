ca396d07b10fd16a187045e9136dbc4eae0437ab
Valentyn Ziomenko <ziomenkow@gmail.com>
2026-09-14 22:15:18 +0200

    final fix

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -7,9 +7,7 @@ export const errorHandler = (err, req, res, next) => {
     });
   }
 
-  const isProd = process.env.NODE_ENV === "production";
-
   res.status(500).json({
-    message: isProd ? err.message : err.stack,
+    message: err.message,
   });
 };
